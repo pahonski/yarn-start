85fbc1dc0401dde884763664211cf28f54ee3b15
Pavel <pahonski@gmail.com>
2018-07-08 21:40:53 +0300

    first commit

---
+++ gulp/tasks/pug.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = function () {
+  $.gulp.task('pug', function () {
+    return $.gulp.src('./source/template/*.pug')
+      .pipe($.gp.pug({
+        pretty: true
+      }))
+      .on('error', $.gp.notify.onError(function (error) {
+        return {
+          title: 'pug',
+          message: error.message
+        }
+      }))
+      .pipe($.gulp.dest('./build/'));
+  })
+};
---
+++ gulp/tasks/serve.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = function () {
+  $.gulp.task('serve', function () {
+    $.browserSync.init({
+      open: false,
+      server: './build'
+    });
+    $.browserSync.watch('build', $.browserSync.reload);
+  });
+};
